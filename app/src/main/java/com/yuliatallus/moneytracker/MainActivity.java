9be673cb64dff147e1900efb94f5fe1beeacb9aa
YuliaTallus <yushestakova@icloud.com>
2015-12-05 21:15:11 +0300

    navigation/toolbar is added

--- app/src/main/java/com/yuliatallus/moneytracker/MainActivity.java
+++ app/src/main/java/com/yuliatallus/moneytracker/MainActivity.java
@@ -1,23 +1,25 @@
 package com.yuliatallus.moneytracker;
 
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = MainActivity.class.getSimpleName();
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupToolbar();
+
         Log.i(TAG, "onCreate");
     }
 
@@ -55,6 +57,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestart(){
        super.onRestart();
         Log.i(TAG,"onRestart");
+    }
+
+    private void setupToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
 
