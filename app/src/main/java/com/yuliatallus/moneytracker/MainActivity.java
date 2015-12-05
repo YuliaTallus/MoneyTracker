6e4ea3b5d38e98f7ec276308c12a0f41578f318b
YuliaTallus <yushestakova@icloud.com>
2015-12-06 00:11:02 +0300

    navigation/navigationView is added

--- app/src/main/java/com/yuliatallus/moneytracker/MainActivity.java
+++ app/src/main/java/com/yuliatallus/moneytracker/MainActivity.java
@@ -2,23 +2,32 @@ package com.yuliatallus.moneytracker;
 
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = MainActivity.class.getSimpleName();
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private CoordinatorLayout coordinatorContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        coordinatorContainer = (CoordinatorLayout)findViewById(R.id.coordinator_container);
 
         setupToolbar();
+        setupDrawer();
 
         Log.i(TAG, "onCreate");
     }
@@ -64,9 +73,23 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null){
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu_white_24dp);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
 
+    private void setupDrawer(){
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigtion_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                Snackbar.make(coordinatorContainer, item.getTitle(), Snackbar.LENGTH_SHORT).show();
+                item.setChecked(true);
+                drawerLayout.closeDrawers();
+                return false;
+            }
+        });
+    }
 
 }
