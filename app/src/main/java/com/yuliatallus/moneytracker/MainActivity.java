2da931998b21d9bb40c4fab9e40a94f823738cd8
YuliaTallus <yushestakova@icloud.com>
2015-12-12 19:42:47 +0300

    Interface/listview is colored, extra code is deleted

--- app/src/main/java/com/yuliatallus/moneytracker/MainActivity.java
+++ app/src/main/java/com/yuliatallus/moneytracker/MainActivity.java
@@ -1,61 +1,101 @@
 package com.yuliatallus.moneytracker;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
+    private DrawerLayout drawerLayout;
+    private Fragment fragment;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG, "onCreate");
+        setupToolbar();
+        setupDrawer();
+        if(savedInstanceState == null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new ExpensesFragment()).commit();
+        }
+    }
+
+    private void setupToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+          }
     }
 
     @Override
-    protected void onStart(){
-        super.onStart();
-        Log.i(TAG, "onStart");
+    public void onBackPressed() {
+
+        Fragment findingFragment = getSupportFragmentManager().findFragmentById(R.id.main_container);
+        if (findingFragment != null && findingFragment instanceof ExpensesFragment) {
+            getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            navigationView.setCheckedItem(R.id.drawer_expenses);
+        }
+        else if (findingFragment instanceof CategoriesFragment) {
+            navigationView.setCheckedItem(R.id.drawer_categories);
+        } else if(findingFragment instanceof StatisticsFragment) {
+            navigationView.setCheckedItem(R.id.drawer_statistics);
+        } else if(findingFragment instanceof SettingsFragment) {
+            navigationView.setCheckedItem(R.id.drawer_settings);
+        }
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
+
+    private void setupDrawer(){
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigtion_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.drawer_expenses:fragment = new ExpensesFragment();
+                        break;
+
+                    case R.id.drawer_categories:fragment = new CategoriesFragment();
+                        break;
+
+                    case R.id.drawer_statistics:fragment = new StatisticsFragment();
+                        break;
+
+                    case R.id.drawer_settings:fragment = new SettingsFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container,fragment).addToBackStack(null).commit();
+                item.setChecked(true);
+                drawerLayout.closeDrawers();
+                return false;
+            }
+        });
+     }
 
     @Override
-    protected void onResume(){
-        super.onResume();
-        Log.i(TAG, "onResume");
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.i(TAG, "onPause");
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.i(TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.i(TAG, "onDestroy");
-    }
-
-    @Override
-    protected void onRestart(){
-       super.onRestart();
-        Log.i(TAG,"onRestart");
-    }
-
-
 }
