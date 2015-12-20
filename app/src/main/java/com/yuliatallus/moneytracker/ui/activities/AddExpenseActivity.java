f3bea2b521bee387ba88a4683bc62f0c4ff63ed9
YuliaTallus <yushestakova@icloud.com>
2015-12-20 03:20:04 +0300

    DB/hometask is done, but code is ugly so far

--- app/src/main/java/com/yuliatallus/moneytracker/ui/activities/AddExpenseActivity.java
+++ app/src/main/java/com/yuliatallus/moneytracker/ui/activities/AddExpenseActivity.java
@@ -2,10 +2,14 @@ package com.yuliatallus.moneytracker.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.Toast;
 
 import com.activeandroid.query.Select;
 import com.yuliatallus.moneytracker.R;
@@ -38,9 +42,37 @@ public class AddExpenseActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle(R.string.add_expense);
 
+        final EditText sumEditText = (EditText)findViewById(R.id.sum_edit_text);
+        final EditText noteEditText = (EditText)findViewById(R.id.note_edit_text);
+        final EditText dateEditText = (EditText)findViewById(R.id.date_edit_text);
+
+        if (sumEditText.getText().toString().equals("")){
+            Toast.makeText(this, "The sum is empty. Fill it, Silly!", Toast.LENGTH_SHORT).show();
+        }
+
+        if (noteEditText.getText().toString().equals("")){
+            Toast.makeText(this, "The note is empty. Fill it, Silly!", Toast.LENGTH_SHORT).show();
+        }
+
+        if (dateEditText.getText().toString().equals("")){
+            Toast.makeText(this, "The date is empty. Fill it, Silly!", Toast.LENGTH_SHORT).show();
+        }
+
         SpinnerAdapter adapter = new MySpinAdapter(this, getDataList());
         Spinner spinner = (Spinner)findViewById(R.id.spinner);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     private List<Categories> getDataList()
--- app/src/main/java/com/yuliatallus/moneytracker/ui/fragments/CategoriesFragment.java
+++ app/src/main/java/com/yuliatallus/moneytracker/ui/fragments/CategoriesFragment.java
@@ -53,38 +53,46 @@ public class CategoriesFragment extends Fragment {
         categoriesRecyclerView.setLayoutManager(linearLayoutManager);
         fabClicked();
         getActivity().setTitle(getString(R.string.nav_drawer_categories));
+        loadData();
     }
 
 
 
-//    private void loadData()
-//    {
-//        getLoaderManager().restartLoader(0, null, new LoaderManager.LoaderCallbacks<List<Categories>>() {
-//            @Override
-//            public Loader<List<Categories>> onCreateLoader(int id, Bundle args) {//import android.support.v4.content.AsyncTaskLoader;
-//                final AsyncTaskLoader<List<Categories>> loader = new AsyncTaskLoader<List<Categories>>(getActivity()) {
-//                    @Override
-//                    public List<Categories> loadInBackground() {
-//                        return getDataList();
-//                    }
-//                };
-//
-//                loader.forceLoad();
-//                return loader;
-//            }
-//
-//            @Override
-//            public void onLoadFinished(Loader<List<Categories>> loader, List<Categories> data) {
-//
-//                categoriesRecyclerView.setAdapter(new CategoriesAdapter(data));
-//            }
-//
-//            @Override
-//            public void onLoaderReset(Loader<List<Categories>> loader) {
-//
-//            }
-//        });
-//    }
+    private void loadData()
+    {
+        getLoaderManager().restartLoader(0, null, new LoaderManager.LoaderCallbacks<List<Categories>>() {
+            @Override
+            public Loader<List<Categories>> onCreateLoader(int id, Bundle args) {//import android.support.v4.content.AsyncTaskLoader;
+                final AsyncTaskLoader<List<Categories>> loader = new AsyncTaskLoader<List<Categories>>(getActivity()) {
+                    @Override
+                    public List<Categories> loadInBackground() {
+                        return getDataList();
+                    }
+                };
+
+                loader.forceLoad();
+                return loader;
+            }
+
+            @Override
+            public void onLoadFinished(Loader<List<Categories>> loader, List<Categories> data) {
+
+                categoriesRecyclerView.setAdapter(new CategoriesAdapter(data));
+            }
+
+            @Override
+            public void onLoaderReset(Loader<List<Categories>> loader) {
+
+            }
+        });
+    }
+
+    private List<Categories> getDataList()
+    {
+        return new Select()
+                .from(Categories.class)
+                .execute();
+    }
 
 
 }
