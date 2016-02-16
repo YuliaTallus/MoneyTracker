83e160f0675a0539f8af250a9020de2d09746f5b
YuliaTallus <yushestakova@icloud.com>
2016-02-17 02:07:53 +0300

    search/ add category request is fixed

--- app/src/main/java/com/yuliatallus/moneytracker/ui/fragments/CategoriesFragment.java
+++ app/src/main/java/com/yuliatallus/moneytracker/ui/fragments/CategoriesFragment.java
@@ -1,5 +1,8 @@
 package com.yuliatallus.moneytracker.ui.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,15 +13,22 @@ import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 
 import com.activeandroid.query.Select;
 import com.yuliatallus.moneytracker.R;
 import com.yuliatallus.moneytracker.adapters.CategoriesAdapter;
 import com.yuliatallus.moneytracker.database.Categories;
+import com.yuliatallus.moneytracker.rest.RestService;
+import com.yuliatallus.moneytracker.rest.model.CreateCategoryModel;
+import com.yuliatallus.moneytracker.ui.activities.LoginActivity_;
 import com.yuliatallus.moneytracker.util.ConstantBox;
 
 import org.androidannotations.annotations.AfterViews;
@@ -36,7 +46,9 @@ import java.util.List;
 @OptionsMenu(R.menu.search_menu)
 public class CategoriesFragment extends Fragment {
 
-    private static final String TAG =CategoriesFragment.class.getSimpleName();
+    private static final String TAG = CategoriesFragment.class.getSimpleName();
+    private static final String OK = "Ок";
+    private static final String CANCEL = "Отмена";
 
     @ViewById(R.id.context_recyclerview)
     RecyclerView categoriesRecyclerView;
@@ -52,7 +64,32 @@ public class CategoriesFragment extends Fragment {
     @Click(R.id.fab)
     void fabClicked() {
         if (getView() != null) {
-            Snackbar.make(getView(), R.string.nice_text, Snackbar.LENGTH_SHORT).show();
+
+            LayoutInflater li = LayoutInflater.from(getContext());
+            View addCatView = li.inflate(R.layout.add_category_layout, null);
+            AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(getContext());
+            mDialogBuilder.setView(addCatView);
+            final EditText userInput = (EditText) addCatView.findViewById(R.id.input_text);
+            mDialogBuilder
+                    .setCancelable(false)
+                    .setPositiveButton(OK,
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,int id) {
+                                    Categories newCat = new Categories(userInput.getText().toString());
+                                    newCat.save();
+                                    addCategory(newCat);
+                                }
+                            })
+                    .setNegativeButton(CANCEL,
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,int id) {
+                                    dialog.cancel();
+                                }
+                            });
+            AlertDialog alertDialog = mDialogBuilder.create();
+
+            alertDialog.show();
+
         }
     }
 
@@ -121,6 +158,29 @@ public class CategoriesFragment extends Fragment {
 
             }
         });
+    }
+
+    @Background
+    void addCategory(Categories category){
+        RestService restService = new RestService();
+        CreateCategoryModel createCategoryModel = restService.createCat(getContext(), category.name);
+        switch (createCategoryModel.getStatus()){
+
+            case ConstantBox.SUCCESS:
+                    Log.d(TAG, "Status: " + createCategoryModel.getStatus() +
+                            ", Title: " + createCategoryModel.getData().getTitle() +
+                            ", Id: " + createCategoryModel.getData().getId());
+                    break;
+
+            case ConstantBox.UNAUTHORIZED:
+                    startActivity(new Intent(getContext(), LoginActivity_.class));
+                    break;
+
+            case ConstantBox.ERROR:
+                    Log.d(TAG, "Ошибка при добавлении категории");
+                    break;
+        }
+
     }
 
 
